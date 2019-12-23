e7685f011453d73e41e50f1be7fca6ec7c7c073f
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2019-12-23 22:15:26 +0800

    #Round609

--- codeforces/#Round609/A.cpp
+++ codeforces/#Round609/A.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 int main(){
+    ios_base::sync_with_stdio(0);
+    ll n;
+    cin >> n;
+    if(n == 1){
+        cout << 9 << " " << 8 << endl;
+    }
+    else
+        cout << n*3 << " " <<n*2 <<endl;
     return 0;
 }
--- codeforces/#Round609/C.cpp
+++ codeforces/#Round609/C.cpp
@@ -1,5 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int maxn = 2010;
 int main(){
+    ios_base::sync_with_stdio(0);
+    int n, m;
+    cin >> n >> m;
+    string s, t;
+    cin >> s;
+    t=s;
+    for(int i=0; i<n; i++){
+        t[i] = s[i%m];
+    }
+    //cout << t << endl;
+    if(t>=s){
+        cout << t.length() << endl;
+        cout << t << endl;
+        return 0;
+    }
+    for(int i=m-1; i>=0; i--){
+        if(t[i] != '9'){
+            t[i]++;
+            for(int j=i+1; j<m; j++){
+                t[j] = '0';
+            }
+            break;
+        }
+    }
+    cout << n << endl;
+    for(int i=0; i<n; i++)
+        cout << t[i%m];
     return 0;
 }
