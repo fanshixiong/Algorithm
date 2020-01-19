8cdfb022380c5302709b3442b6c5ee0ab8a57632
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-01-19 14:39:02 +0800

    #Round

--- codeforces/#Round613/E.cpp
+++ codeforces/#Round613/E.cpp
@@ -1,10 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int maxn = 3e5+7;
-ll p[maxn];
+const int maxn = 4e5+7;
+int n;
+pair<int, int> a[maxn];
+int ans[maxn];
+void solve(){
+    cin >> n;
+    for (int i = 1; i <= n; i++){
+        int l, r;
+        cin >> l >> r;
+        a[(i << 1) - 1] = make_pair(l, -i);
+        a[i << 1] = make_pair(r, i);
+        ans[i] = 0;
+    }
+    sort(a + 1, a + 1 + (n << 1));
+    multiset<int> s;
+    int c = 0;
+    for (int i = 1; i <= 2 * n; i ++){
+        if(a[i].second<0)
+            s.insert(-a[i].second);
+        else
+            s.erase(s.find(a[i].second));
+        if(s.size() == 0)
+            c++;
+        if(s.size() == 1 && a[i].second > 0 && a[i+1].second < 0 && a[i+1].first > a[i].first)
+            ans[*s.begin()]++;
+        if(s.size() == 1 && a[i].second<0 && a[i+1].second>0)
+            ans[*s.begin()]--;
+    }
+    int res = -1;
+    for (int i = 1; i<=n; i++){
+        res = max(res, ans[i]);
+    }
+    cout << res + c << endl;
+}
 int main(){
     ios_base::sync_with_stdio(0);
-    ll n, m, ans=0;
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
     return 0;
 }
--- codeforces/Educational Codeforces Round 80/E.cpp
+++ codeforces/Educational Codeforces Round 80/E.cpp
@@ -1,10 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int maxn = 3e5+7;
-ll p[maxn];
+const int maxn = 1e6+7;
+
+int bit[maxn], a[maxn], max_a[maxn], min_a[maxn];
+int pos[maxn];
+int n, m;
+
+int lowbit(int x){
+    return x & (-x);
+}
+
+void update(int x, int op){
+    while(x < maxn){
+        bit[x] += op;
+        x += lowbit(x);
+    }
+}
+int get(int x){
+    int ans=0;
+    while(x){
+        ans += bit[x];
+        x -= lowbit(x);
+    }
+    return ans;
+}
 int main(){
     ios_base::sync_with_stdio(0);
-    ll n, m, ans=0;
-    return 0;
+    cin >> n >> m;
+    memset(bit, 0, sizeof(bit));
+    for (int i = 1; i<=n; i++){
+        max_a[i] = min_a[i] = i;
+        pos[i] = i + m;
+        update(i+m, 1);
+    }
+    for (int i = 0; i<m; i++){
+        int x;
+        cin >> x;
+        min_a[x] = 1;
+        max_a[x] = max(max_a[x], get(pos[x]));
+
+        update(pos[x], -1);
+        pos[x] = m - i;
+        update(pos[x], 1);
+    }
+    for (int i = 1; i<=n; i++){
+        max_a[i] = max(max_a[i], get(pos[i]));
+    }
+    for (int i = 1; i <= n; i++){
+        cout << min_a[i] << " " << max_a[i] << endl;
+    }
+        return 0;
 }
