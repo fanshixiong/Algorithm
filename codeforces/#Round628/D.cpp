3b5316e270da91d37f011e2f057c655b6f089af9
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-19 21:39:18 +0800

    #Round628

--- codeforces/#Round628/D.cpp
+++ codeforces/#Round628/D.cpp
@@ -1,10 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-int main(){
-    ios_base::sync_with_stdio();
+void solve(){
     ll u, v;
     cin >> u >> v;
-
+    if(u > v || ((v - u) & 1)){
+        cout << -1 << endl;
+        return;
+    }
+    if(u == v){
+        if(!u)
+            cout << 0 << endl;
+        else{
+            cout << 1 << endl;
+            cout << u << endl;
+        }
+        return;
+    }
+    ll x = (v - u) / 2, y = (v + u) / 2;
+    if((x^y) == u && (x+y) == v){
+        cout << 2 << endl;
+        cout << x << " " << y << endl;
+    }
+    else{
+        cout << 3 << endl;
+        cout << u << " " << x << " " << x << endl;
+    }
+}
+int main(){
+    ios_base::sync_with_stdio();
+    solve();
     return 0;
 }
