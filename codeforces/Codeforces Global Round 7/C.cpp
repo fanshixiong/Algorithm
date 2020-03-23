ea76631cf5feb384c37580005f6fdeb64140ed77
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-23 15:17:11 +0800

    Global 7

--- codeforces/Codeforces Global Round 7/C.cpp
+++ codeforces/Codeforces Global Round 7/C.cpp
@@ -1,6 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
+
+const int maxn = 2e5 + 10;
+const int mod = 998244353;
+pair<int, int> p[maxn];
+int cnt[maxn];
+
 int main(){
     ios_base::sync_with_stdio(0);
+    int n, k; cin >> n >> k;
+    for (int i = 1; i <= n; i++){
+        cin >> p[i].first;
+        p[i].second = i;
+    }
+    sort(p + 1, p + 1 + n);
+    ll sum = 0, ans = 1;
+    for (int i = n; i > n - k; i--){
+        cnt[n - i + 1] = p[i].second;
+        sum += p[i].first;
+    }
+    sort(cnt + 1, cnt + 1 + k);
+    for (int i = 2; i <= k; i++){
+        ans = (ans * (cnt[i] - cnt[i - 1])) % mod;
+    }
+    cout << sum << " " << ans << endl;
     return 0;
 }
