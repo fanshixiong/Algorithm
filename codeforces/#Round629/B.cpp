6f69c556c5bb1a705a6061de77c85f94d9f15e1b
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-29 21:17:07 +0800

    #Round629

--- codeforces/#Round629/B.cpp
+++ codeforces/#Round629/B.cpp
@@ -1,16 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-void solve(){
-    int n, k;
-    
-}
+const int maxn = 1e5 + 10;
+int cnt[maxn], sum[maxn];
 int main(){
     ios_base::sync_with_stdio(0);
     int t;
     cin >> t;
-    while(t--){
-        solve();
+    for (int i = 2; i < maxn; i++){
+        cnt[i] = cnt[i - 1] + 1;
+        sum[i] = sum[i - 1] + cnt[i];
+    }
+    while (t--)   {
+        int n, k, i;
+        cin >> n >> k;
+        for (i = 2; i <= n; i++){
+            if(sum[i] >= k) break;
+        }
+        int j = i; k -= sum[j-1];
+        string s;
+        cout << j << " " << k << endl;
+        for (int i = 0; i < n; i++) s += 'a';
+        s[n - j] = 'b';
+        s[n - k] = 'b';
+        cout << s << endl;
     }
     return 0;
 }
