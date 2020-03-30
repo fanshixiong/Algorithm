9124302e075e1d0b14947636db4f0be161a60654
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-30 17:39:08 +0800

    #Round 629

--- codeforces/#Round629/B.cpp
+++ codeforces/#Round629/B.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 #define ll long long
 const int maxn = 1e5 + 10;
-int cnt[maxn], sum[maxn];
+ll cnt[maxn], sum[maxn];
 int main(){
     ios_base::sync_with_stdio(0);
     int t;
     cin >> t;
+    memset(cnt, 0, sizeof(cnt));
+    memset(sum, 0, sizeof(cnt));
     for (int i = 2; i < maxn; i++){
         cnt[i] = cnt[i - 1] + 1;
         sum[i] = sum[i - 1] + cnt[i];
@@ -19,7 +21,7 @@ int main(){
         }
         int j = i; k -= sum[j-1];
         string s;
-        cout << j << " " << k << endl;
+        //cout << j << " " << k << endl;
         for (int i = 0; i < n; i++) s += 'a';
         s[n - j] = 'b';
         s[n - k] = 'b';
--- codeforces/#Round629/C.cpp
+++ codeforces/#Round629/C.cpp
@@ -1,6 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
+void solve(){
+    int n;
+    string s;
+    cin >> n >> s;
+    string t1, t2;
+    bool flg = 0;
+    for (int i = 0; i < n; i++){
+        if(s[i] == '0'){
+            t1 += '0'; t2 += '0';
+        }
+        else if(s[i] == '1'){
+            if(!flg){
+                t1 += '1'; t2 += '0'; flg = 1;
+            }
+            else{
+                t1 += '0'; t2 += '1';
+            }
+        }
+        else{
+            if(flg){
+                t2 += '2'; t1 += '0';
+            }
+            else{
+                t1 += '1'; t2 += '1';
+            }
+        }
+    }
+    cout << t1 << endl << t2 << endl;
+}
 int main(){
     ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
     return 0;
 }
