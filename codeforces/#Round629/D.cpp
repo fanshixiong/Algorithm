32023c2d37661c6c068b41c6102fa438f16992ae
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-30 17:44:46 +0800

    #Round 629

---
+++ codeforces/#Round629/D.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int maxn = 2e5 + 10;
+int t[maxn];
+void solve(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++){
+        cin >> t[i];
+    }
+}
+int main(){
+    ios_base::sync_with_stdio(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
