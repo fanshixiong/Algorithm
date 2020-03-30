a683eda4028f7c7586e26e7b113496f712a26d0f
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-03-30 16:03:13 +0800

    Round75

---
+++ codeforces/Round75/cmake-build-debug/CMakeFiles/Round75.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Round75.dir/main.cpp.obj"
+  "Round75.exe"
+  "Round75.exe.manifest"
+  "Round75.pdb"
+  "libRound75.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Round75.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
