a1b63cc4b3b7008267fa6546be0b7c8108e8c888
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-04-03 21:53:47 +0800

    #Round 630

--- codeforces/#Round630/B.cpp
+++ codeforces/#Round630/B.cpp
@@ -3,9 +3,32 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+int c[maxn], col[1010];
+int prime[11] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31};
 void solve(){
     int n;
     cin >> n;
+    map<int, int> p;
+    p.clear();
+    int cnt = 0;
+    for (int i = 0; i < n; i++){
+        cin >> c[i];
+        for (int j = 0; j < 11; j++){
+            if(c[i] % prime[j] == 0){
+                if(!p[prime[j]]){
+                    p[prime[j]] = ++ cnt;
+                }
+                //cout << i << " " << c[i] << " "<< prime[j] << endl;
+                col[i] = p[prime[j]];
+                break;
+            }
+        }
+    }
+    cout << cnt << endl;
+    for (int i = 0; i < n; i++){
+        cout << col[i] << " ";
+    }
+    cout << endl;
 }
 int main(){
     IOS;
