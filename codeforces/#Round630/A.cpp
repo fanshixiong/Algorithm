4f67cdedcdf80e104593456c45d28c366e881eb1
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-04-03 17:06:23 +0800

    #Round 630

--- codeforces/#Round630/A.cpp
+++ codeforces/#Round630/A.cpp
@@ -4,8 +4,21 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
 void solve(){
-    int n;
-    cin >> n;
+    int a, b, c, d;
+    int x, y, x1, y1, x2, y2;
+    cin >> a >> b >> c >> d;
+    cin >> x >> y >> x1 >> y1 >> x2 >> y2;
+    if((x1 == x2 && (a | b)) || (y1 == y2) && (c | d)){ 
+        cout << "NO" << endl;
+        return;
+    }
+    int xx = b - a + x, yy = d - c + y;
+    if(xx < x1 || xx > x2 || yy < y1 || yy > y2){
+        cout << "NO" << endl;
+    }
+    else{
+        cout << "YES" << endl;
+    }
 }
 int main(){
     IOS;
