97f92711e9d9ebcaa43fc020b1e5c99a42d0f6a4
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-04-14 14:25:18 +0800

    模板

--- ban/数位dp.cpp
+++ ban/数位dp.cpp
@@ -1,7 +1,7 @@
 typedef long long ll;
 int a[20];
 ll dp[20][state];//不同题目状态不同
-ll dfs(int pos,/*state变量*/,bool lead/*前导零*/,bool limit/*数位上界变量*/)//不是每个题都要判断前导零
+ll dfs(int pos, int state/*state变量*/,bool lead/*前导零*/,bool limit/*数位上界变量*/)//不是每个题都要判断前导零
 {
     //递归边界，既然是按位枚举，最低位是0，那么pos==-1说明这个数我枚举完了
     if(pos==-1) return 1;/*这里一般返回1，表示你枚举的这个数是合法的，那么这里就需要你在枚举时必须每一位都要满足题目条件，也就是说当前枚举到pos位，一定要保证前面已经枚举的数位是合法的。不过具体题目不同或者写法不同的话不一定要返回1 */
