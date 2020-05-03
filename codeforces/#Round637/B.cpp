9574e69c830b4b9b4875f39a63573297f59160e8
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-03 16:04:39 +0800

    #Round 637

--- codeforces/#Round637/B.cpp
+++ codeforces/#Round637/B.cpp
@@ -3,8 +3,28 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+ll a[maxn], p[maxn], pos[maxn];
 void solve(){
-    int n; cin >> n;
+    int n, k; cin >> n >> k;
+    for (int i = 0; i <= n; i++) p[i] = pos[i] = 0;
+    for (int i = 0; i < n; i++)  cin >> a[i];
+    int l = n - 1;
+    for (int i = 1; i < n - 1; i++){
+        if(a[i] > a[i-1] && a[i] > a[i+1]) pos[i] = 1;
+    }
+    //for (int i = 0; i < n; i++) cout << pos[i] << " ";
+    //cout << endl;
+    ll peak = 0, ans = -1, left = -1;
+    for (; l >= 0; l--){
+        if(pos[l]) peak++;
+        if(l + k - 1 < n && pos[l + k - 1]) peak--;
+
+        if(ans <= peak){
+            ans = peak;
+            left = l;
+        }
+    }
+    cout << ans + 1 << " " << left + 1 << endl;
 }
 int main(){
     IOS; int t; cin >> t;
