a6380cec55301888aba40ed47ff52f318968d258
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-06 22:07:30 +0800

    #Round 637

--- codeforces/#Round637/D.cpp
+++ codeforces/#Round637/D.cpp
@@ -4,10 +4,11 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
 void solve(){
-    int n; cin >> n;
+    int n, k; cin >> n >> k;
+    
 }
 int main(){
-    IOS; int t; cin >> t;
+    IOS; int t = 1;
     while(t--){
         solve();
     }
