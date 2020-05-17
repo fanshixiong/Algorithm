bd80bdc0cae7dd0fd6a2feb8ae68f445fbe013cf
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-17 20:47:46 +0800

    #Round 642

--- codeforces/#Round642/D.cpp
+++ codeforces/#Round642/D.cpp
@@ -3,8 +3,27 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+int a[maxn];
+struct cmp{
+    bool operator()(const pair<int, int> p1, const pair<int, int> p2){
+        if(p1.second - p1.first == p2.second - p2.first) return p1.first > p2.first;
+        return p1.second - p1.first < p2.second - p2.first;
+    }
+};
+priority_queue<pair<int, int>, vector<pair<int, int> >, cmp> q;
 void solve(){
-    int n, m; cin >> n >> m;
+    int n, m; cin >> n;
+    q.push(make_pair(1, n));
+    int cnt = 0;
+    while(!q.empty()){
+        pair<int, int> p = q.top(); q.pop();
+        int mid = (p.second + p.first) / 2;
+        a[mid] = ++cnt;
+        if(mid - p.first >= 1) q.push(make_pair(p.first, mid-1));
+        if(p.second - mid >= 1) q.push(make_pair(mid+1, p.second));
+    }
+    for (int i = 1; i <= n; i++) cout << a[i] << " ";
+    cout << endl;
 }
 int main(){
     IOS; int t; cin >> t;
