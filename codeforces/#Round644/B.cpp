52602e5b6317aa7c14ca327036e5d2f689c118b3
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-30 22:25:09 +0800

    #Round 644

--- codeforces/#Round644/B.cpp
+++ codeforces/#Round644/B.cpp
@@ -1,15 +1,57 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 2e5 + 10;
-void solve(){
-    int n; cin >> n;
+bool AllisNum(string str)
+{
+	for (int i = 0; i < str.size(); i++)
+	{
+		int tmp = (int)str[i];
+		if (tmp >= 48 && tmp <= 57)
+		{
+			continue;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
 }
+bool isNum(string a) {
+	string s = "";	
+	//cout << a << endl;
+	for (int i = 0; i < a.size(); i++) {
+		if (a[i] != ' ' && a[i] != '\n' && a[i] != '\t' && a[i] != ';' && a[i] != '[' && a[i] != ']')
+			s = s + a[i];
+		else {
+			if (AllisNum(s)) {
+			//	cout << s << endl;
+				return true;
+			}
+			s = "";
+		}
+	}
+	return false;
+}
+//判读是否是有变量定义且含有常数
+string varType[7] = {"int", "double", "float", "byte", "char", "long", "void"}; //变量类型
+
+bool isVar(string a) {
+	//cout << a << endl;
+	for (int i = 0; i < 7; i++) {
+		string::size_type idx;
+		idx = a.find(varType[i]);
+		if (idx != string::npos) {
+			if (isNum(a)) {
+				cout << a << endl;
+				return true;
+			}
+		}	
+	}
+	return false;
+}
+
 int main(){
-    IOS; int t; cin >> t;
-    while(t--){
-        solve();
-    }
+    string a = "int a = 12;";
+    cout << isVar(a) << endl;
     return 0;
 }
--- codeforces/#Round644/B1.cpp
+++ codeforces/#Round644/B1.cpp
@@ -3,12 +3,16 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+int a[maxn];
 void solve(){
-    int a, b; cin >> a >> b;
-    if(a < b) swap(a, b);
-    b *= 2;
-    if(a < b) cout << b * b << endl;
-    else  cout << a * a << endl;
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a + n);
+    int ans = 0x3f3f3f3f;
+    for (int i = 1; i < n; i++){
+        ans = min(a[i] - a[i - 1], ans);
+    }
+    cout << ans << endl;
 }
 int main(){
     IOS; int t; cin >> t;
