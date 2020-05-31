f86e06f0feec3ded82e6a69af2a5db6adb21d473
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-31 11:29:53 +0800

    #Round 644

--- codeforces/#Round644/C.cpp
+++ codeforces/#Round644/C.cpp
@@ -3,8 +3,33 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+int a[maxn];
 void solve(){
     int n; cin >> n;
+    int e = 0, o = 0;
+    for (int i = 0; i < n; i++){
+        cin >> a[i];
+        if(a[i] % 2 == 0){
+            e++;
+        }
+        else o++;
+    }
+    if(e % 2 != o % 2){
+        cout << "NO" << endl;
+        return;
+    }
+    if(e % 2 == 0){
+        cout << "YES" << endl;
+        return;
+    }
+    sort(a, a + n);
+    for (int i = 1; i < n; i++){
+        if(a[i] % 2 != a[i-1] % 2  && a[i]-a[i-1] == 1){
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
 }
 int main(){
     IOS; int t; cin >> t;
