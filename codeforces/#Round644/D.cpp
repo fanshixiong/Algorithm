b17f4537ed94ad261be864d7ef25aa511970b13e
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-05-31 12:02:52 +0800

    #Round 644

--- codeforces/#Round644/D.cpp
+++ codeforces/#Round644/D.cpp
@@ -4,7 +4,16 @@ using namespace std;
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
 void solve(){
-    int n; cin >> n;
+    int n, k; cin >> n >> k;
+    int ans = n;
+    for (int j = 1; j * j <= n; j++) {
+		if (n % j == 0) {
+			if (j <= k) ans = min(ans, n / j);
+			if (n / j <= k) ans = min(ans, j);
+		}
+    }
+    
+    cout << ans << endl;
 }
 int main(){
     IOS; int t; cin >> t;
