7299deaaf726da775cfa5b9a069c6a53b1db820c
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-06-09 16:50:19 +0800

    #Round647

--- codeforces/#Round647/D.cpp
+++ codeforces/#Round647/D.cpp
@@ -3,10 +3,38 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e6 + 10;
-ll d[maxn], day_sum[maxn], score_sum[maxn];
+int a[maxn], p[maxn], ans[maxn];
+vector<int> G[maxn];
+bool cmp(int x, int y){
+    return a[x] < a[y];
+}
 void solve(){
-    ll n, x; cin >> n >> x;
-   
+    int n, m; cin >> n >> m;
+    for (int i = 0; i < m; i++){
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    for (int i = 1; i <= n; i++) p[i] = i, cin >> a[i];
+    sort(p + 1, p + n + 1, cmp);
+    for (int i = 1; i <= n; i++){
+        int u = p[i]; //待求的点
+        int val = a[u];  //待求的点的期望的值
+        set<int> s;
+        for(auto x : G[u]){ //将已填好值的点加进去
+            if(ans[x])  s.insert(ans[x]);
+        }
+
+        if(s.size() != val-1){cout << -1 << endl; return;}
+        int cnt = 1;
+        for(auto x : s){
+            if(cnt != x){cout << -1 << endl; return;}
+            cnt++;
+        }
+        ans[u] = a[u];
+    }
+    for (int i = 1; i <= n; i++) cout << p[i] << " ";
 }
 int main(){
     IOS; int t = 1;
