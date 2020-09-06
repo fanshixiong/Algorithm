b8bf687eda90695a0582972bfdd1acc451d48571
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-09-06 11:52:06 +0800

    #Round667

--- codeforces/#Round667/D.cpp
+++ codeforces/#Round667/D.cpp
@@ -5,14 +5,7 @@ using namespace std;
 const int maxn = 2e5 + 10;
 void solve(){
     int n; cin >> n;
-    vector<int> a(n);
-    for(int &x : a) cin >> x;
 
-    int maxPile = *max_element(a.begin(), a.end());
-    int sumStones = accumulate(a.begin(), a.end(), 0);
-
-    if(maxPile*2 <= sumStones && ((sumStones & 1) == 0)) cout << "HL" << endl;
-    else cout << "T" << endl;
 }
 int main(){
     IOS; int t; cin >> t;
