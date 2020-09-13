45538f5988fcb5eb141a1630e519cef92d11647e
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-09-13 08:53:30 +0800

    #Round669

--- codeforces/#Round669/C.cpp
+++ codeforces/#Round669/C.cpp
@@ -3,11 +3,28 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
-ll a[maxn];
+int ans[maxn];
 void solve(){
-    int n, k;
-    cin >> n >> k;
-   
+    int n;
+    cin >> n;
+    int p = 1;
+    for (int i = 2; i < n; i++){
+        int a, b;
+        cout << "? " << p << " " << i << endl;
+        cin >> a;
+        cout << "? " << i << " " << p << endl;
+        cin >> b;
+        if(a < b){
+            ans[i] = b;
+        }else{
+            ans[p] = a;
+            p = i;
+        }
+    }
+    ans[p] = n;
+    cout << "! ";
+    for (int i = 1; i <= n; i++) cout << ans[i] << " ";
+    cout << endl;
 }
 int main(){
     IOS; int t; cin >> t;
