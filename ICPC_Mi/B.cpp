67f12055ecfedd1fd4d2a8e1002e7f662f00c10c
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-10-25 12:18:26 +0800

    ICPC_mi

---
+++ ICPC_Mi/B.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+const int maxn = 2e5 + 10;
+void solve(){
+    int n; cin >> n;
+}
+int main(){
+    IOS; int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
