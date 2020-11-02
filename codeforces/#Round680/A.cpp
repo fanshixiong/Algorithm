66275a5e4fad8fd66f7b596961e0e1770f5b9713
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-11-02 08:28:31 +0800

    #Round680

--- codeforces/#Round680/A.cpp
+++ codeforces/#Round680/A.cpp
@@ -3,26 +3,23 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
-int a[maxn];
+int a[maxn], b[maxn];
 void solve(){
-    int n; cin >> n;
-    vector<int> cnt(101, 0);
+    int n, x; cin >> n >> x;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
+
+    sort(a, a + n);
+    sort(b, b + n);
+
     for (int i = 0; i < n; i++){
-        int x; cin >> x;
-        cnt[x]++;
-    }
-    int ans = -1;
-    for (int i = 0; i <= 100; i++){
-        if(cnt[i] == 1){
-            if(ans == -1) ans = i;
-        }
-        if(cnt[i] == 0){
-            if(ans == -1) ans = i+i;
-            else ans += i;
-            break;
+        int p = n - i - 1;
+        if(a[i] + b[p] > x){
+            cout << "NO" << endl;
+            return;
         }
     }
-    cout << ans << endl;
+    cout << "YES" << endl;
 }
 int main(){
     IOS; int t; cin >> t;
--- codeforces/#Round680/B.cpp
+++ codeforces/#Round680/B.cpp
@@ -3,45 +3,26 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
-int a[maxn], b[maxn];
+int a[maxn];
 void solve(){
     int n; cin >> n;
-    for (int i = 0; i < n; i++) cin >> a[i];
-    memset(b, 0, sizeof b);
-
-    int idx = 0, maxEle = 0;
+    vector<int> cnt(101, 0);
     for (int i = 0; i < n; i++){
-        if(a[i] > maxEle){
-            maxEle = a[i];
-            idx = i;
+        int x; cin >> x;
+        cnt[x]++;
+    }
+    int ans = -1;
+    for (int i = 0; i <= 100; i++){
+        if(cnt[i] == 1){
+            if(ans == -1) ans = i;
+        }
+        if(cnt[i] == 0){
+            if(ans == -1) ans = i+i;
+            else ans += i;
+            break;
         }
     }
-
-    b[0] = maxEle;
-    a[idx] = 0;
-    int g = maxEle;
-
-    for (int i = 1; i < n; i++){
-        maxEle = idx = 0;
-        for (int j = 0; j < n; j++){
-            if(!a[j])continue;
-
-            int t = __gcd(g, a[j]);
-            //cout << t << " ";
-            if(t > maxEle){
-                maxEle = t;
-                idx = j;
-                //cout << t << " " << idx << endl;
-            }
-        }
-        //cout << endl;
-        b[i] = a[idx];
-        a[idx] = 0;
-        g = maxEle;
-    }
-
-    for (int i = 0; i < n; i++) cout << b[i] << " ";
-    cout << endl;
+    cout << ans << endl;
 }
 int main(){
     IOS; int t; cin >> t;
