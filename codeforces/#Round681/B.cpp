8699e477fb80139a318d9a29c9d161e75d4fe7d8
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-11-04 22:17:44 +0800

    #Round681

--- codeforces/#Round681/B.cpp
+++ codeforces/#Round681/B.cpp
@@ -5,10 +5,26 @@ using namespace std;
 const int maxn = 2e5 + 10;
 int a[maxn];
 void solve(){
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    cout << max(a + b, c + d) << endl;
-}
+    int a, b;
+    cin >> a >> b;
+    string s; cin >> s;
+
+    vector<int> p;
+    for (int r = 0; r < (int)s.size(); r++){
+        if(s[r] == '1') p.push_back(r);
+    }
+    if((int)p.size() == 0){
+        cout << 0 << endl;
+        return;
+    }
+    
+    ll sum = a;
+    for (int i = 1; i < (int)p.size(); i++){
+        if(p[i]-p[i-1] <= 1) continue;
+        sum += min((p[i]-p[i-1]-1)*b, a);
+    }
+    cout << sum << endl;
+}  
 int main(){
     IOS; int t; cin >> t;
     while(t--){
