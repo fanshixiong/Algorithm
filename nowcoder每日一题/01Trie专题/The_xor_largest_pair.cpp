7d80b12f93a2e8f1661cb17cd372041ab2a4da3d
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-11-17 21:48:58 +0800

    01Trie专题

---
+++ nowcoder每日一题/01Trie专题/The_xor_largest_pair.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 2e6 + 100;
+int ch[maxn][2];
+
+int sz = 0, rt, n;
+
+int read() {int x; scanf("%d", &x); return x;}
+
+void insert(int u, int t, int x){
+    if(t < 0) return;
+    int f = ((x >> t) & 1);
+    if(!ch[u][f]) ch[u][f] = ++sz;
+    insert(ch[u][f], t-1, x);
+}
+
+int ask(int u, int t, int x){
+    if(t < 0) return 0;
+    int f = ((x >> t) & 1);
+    if(ch[u][!f]) return ask(ch[u][!f], t-1, x) + (1 << t);
+    
+    return ask(ch[u][f], t-1, x);
+}
+
+int main(){
+    n = read();
+    int ans = 0;
+    rt = ++ sz;
+    for(int i = 0; i < n; i++){
+        int x = read();
+        insert(rt, 30, x);
+        if(i != 0) ans = max(ans, ask(rt, 30, x));
+    }
+    printf("%d\n", ans);
+    return 0;
+}
---
+++ nowcoder每日一题/01Trie专题/奶牛异或.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn = 2e7 + 100;
+int ch[maxn][2], id[maxn], a[maxn];
+
+int sz = 0, rt, n;
+int ans1, ans2 = 1, ans3 = 1;
+
+int read() {int x; scanf("%d", &x); return x;}
+
+void insert(int u, int t, int x, int ID){
+    if(t < 0){ id[u] = ID; return; }
+    int f = ((x >> t) & 1);
+    if(!ch[u][f]) ch[u][f] = ++sz;
+    insert(ch[u][f], t-1, x, ID);
+}
+
+int ask(int u, int t, int x){
+    if(t < 0) return id[u];
+    int f = ((x >> t) & 1);
+    if(ch[u][!f]) return ask(ch[u][!f], t-1, x);
+    
+    return ask(ch[u][f], t-1, x);
+}
+
+int main(){
+    n = read(); a[0] = 0;
+    for(int i = 1; i <= n; i++) (a[i] = a[i-1] ^ read());
+    rt = ++ sz;
+    for(int i = 1; i <= n; i++){
+        insert(rt, 22, a[i-1], i);
+        int x = ask(rt, 22, a[i]) - 1;
+        if(ans1 < (a[x] ^ a[i])){
+            ans1 = (a[x] ^ a[i]);
+            ans2 = x + 1;
+            ans3 = i;
+        }
+    }
+    printf("%d %d %d\n", ans1, ans2, ans3);
+    return 0;
+}
