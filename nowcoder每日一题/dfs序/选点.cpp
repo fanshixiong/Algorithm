01cf230946f4d48db163f501adea3f4f695384e7
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-11-25 21:28:35 +0800

    nowcoder 选点

--- nowcoder每日一题/dfs序/选点.cpp
+++ nowcoder每日一题/dfs序/选点.cpp
@@ -9,34 +9,32 @@ using namespace std;
 const int mod = 1e9 + 7;
 const int maxn = 2e5 + 10;
 
-int a[maxn], cnt = 0;
-int l[maxn], r[maxn];
+int a[maxn], tot = 0, cnt;
+int l[maxn], r[maxn], w[maxn], ans[maxn];
 vector<int> G[maxn];
 
-void dfs(int u, int fa){
-    l[u] = ++cnt;
-    a[cnt] = u;
-    for(auto v : G[u]) {
-        if(fa == v) continue;
-        dfs(v, u);
-    }
-    r[u] = cnt;
+void dfs(int u){
+    if(u == 0) return;
+    a[++tot] = w[u];
+    dfs(r[u]); dfs(l[u]);
 }
 
 int main(){
-    int n, q;
-    cin >> n >> q;
-    for(int i = 2; i <= n; i++) {
-        int u; cin >> u;
-        G[i].push_back(u);
-        G[u].push_back(i);
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) cin >> w[i]; 
+    for(int i = 1; i <= n; i++) cin >> l[i] >> r[i];
+
+    dfs(1);
+    ans[++cnt] = a[1];
+    // 最长上升子序列贪心解法
+    for(int i = 2; i <= n; i++){
+        if(a[i] > ans[cnt]) ans[++cnt] = a[i];
+        else{
+            int t = lower_bound(ans + 1, ans + 1 + cnt, a[i]) - ans;
+            ans[t] = a[i];
+        }
     }
-    dfs(1, 0);
-    for(int i = 1; i <= q; i++){
-        int u, k;
-        cin >> u >> k;
-        if(l[u] + k - 1 > r[u]) cout << -1 << endl;
-        else cout << a[l[u] + k - 1] << endl;
-    }
+    cout << cnt << endl;
     return 0;
 }
