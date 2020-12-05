085ef43b18aafddaba187f3926729e41d45fa8b2
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-05 21:46:48 +0800

    nowcoder S2-6

--- codeforces/#Round687/E.cpp
+++ codeforces/#Round687/E.cpp
@@ -1,135 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
-struct OP
-{
-    int pos;
-    char op;
-};
-bool scanString(string str) {  //扫描括号是否匹配
-        if(str=="")
-            return false;
-        stack<string> sta;
-        for (int i = 0; i < str.length(); i++) {
-            if (str[i] == '(')
-                sta.push("(");
-            if (str[i] == ')') {
-                if (!sta.empty() && sta.top() == "(") {
-                    sta.pop();
-                    continue;
-                }
-                else
-                    return false;
-            }
-        }
-        if (sta.empty())
-            return true;
-        else
-            return false;
-    }
-bool judgeString(string str)  //判断是否出现“）（”的情况
-    {
-        int i1=0; int i2=1;
-        while(i2<str.length()){
-            if(str[i1]==')'&&str[i2]=='(')
-                return false;
-            else
-            {   i1++;
-                i2++;
-            }
-        }
-        return true;
-    }
-char * Remove(char * str)
-{
-    char * p = str;
-    int i = 0;
-    stack<OP> ops;
-    vector<int> rems;
-    while(*p!='\0')
-    {
-        OP op;
-        if(*p=='+'||*p=='-'||*p=='*'||*p=='/'||*p=='(')
-        {
-            op.op =*p;
-            op.pos = i;
-            ops.push(op);
-        }
-        else if(*p==')')
-        {
-            int next = 0;
-            int pre  = 0;
-            int lower = 1;
-
-            char * t = p+1;
-            while(*t!='\0'&&*t==' ')
-            {
-                ++t;
-            }
-            next = (*t=='*'||*t=='/');
-
-            while(ops.size()>0)
-            {
-                op = ops.top();
-                ops.pop();
-                if(op.op=='(')
-                {
-                    break;
-                }
-                if(op.op=='+'||op.op=='-')
-                {
-                    lower = 0;
-                }
-            }
-            t =&str[op.pos] -1;
-            while(t>=str&&*t==' ')
-            {
-                --t;
-            }
-            pre = (t>=str&&(*t=='*'||*t=='/'));
-
-            if(lower>=pre&&lower>=next)
-            {
-                rems.push_back(i);
-                rems.push_back(op.pos);
-            }
-        }
-        ++i;
-        ++p;
-    }
-    if(rems.size()==0)
-    {
-        return str;
-    }
-    sort(rems.begin(),rems.end());
-    int cur = rems[0];
-    int beg = rems[0] +1;
-    for(int i = 0;i<rems.size()-1;++i)
-    {
-        int end = rems[i+1];
-        while(beg<end)
-        {
-            str[cur]=  str[beg];
-            ++beg;
-            ++cur;
-        }
-        beg = end +1;
-    }
-    while(str[cur]!='\0')
-    {
-        str[cur]=  str[beg];
-        ++cur;
-        ++beg;
-    }
-    return str;
-}
-
-int main()
-{   char str[256]= "((1)+(2/2)+(1*2)/2)";
-    if(scanString(str)!=true||judgeString(str)!=true)
-      cout<<"error!!"<<endl;
+const int nmax=1010;
+int father[nmax];
+int isRoot[nmax];
+ 
+int findFather(int u){
+    if(u==father[u]) return u;
     else{
-
-            cout<<Remove(str)<<endl;
+        int f=findFather(father[u]);
+        father[u]=f;
+        return f;
     }
+} 
+ 
+void Union(int u,int v){
+    int fu=findFather(u);
+    int fv=findFather(v);
+    if(fu!=fv){
+        father[fu]=fv;
+    } 
+}
+ 
+void init(int n){
+    for(int i=1;i<=n;i++){
+        father[i]=i;
+        isRoot[i]=0;
+    }
+}
+ 
+int main(int argc, char** argv) {
+    int n,m;//点数，边数
+    while(cin>>n>>m){
+        if(n==0){
+            break;
+        }
+        memset(father,0,sizeof(father));
+        memset(isRoot,0,sizeof(isRoot));
+        init(n);
+        int u,v;
+        for(int i=0;i<m;i++){
+            cin>>u>>v;
+            Union(u,v);
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(father[i]==i){
+                ans++;
+            }
+        }
+        if(ans==1){
+            printf("YES\n");
+        }else{
+            printf("NO\n");
+        }
+    } 
     return 0;
 }
