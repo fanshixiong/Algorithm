8dce99c2ef29efc7e4d6050d7712dc032f9416ee
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-23 17:32:34 +0800

    nc

--- Test.cpp
+++ Test.cpp
@@ -2,32 +2,67 @@
 using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 2e5 + 10;
 
+#define pb push_back
+const int maxn = 5e3+10;
+vector<int> G[maxn];
+int vis[maxn];
+int tans, s;
 class Solution {
 public:
     /**
      * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
      * 
      * @param n int整型 
-     * @param k int整型 
-     * @param Point int整型vector 
-     * @return int整型vector
+     * @param u int整型vector 
+     * @param v int整型vector 
+     * @return int整型
      */
-    vector<int> city(int n, int k, vector<int>& Point) {
+    void dfs(int u, int dep){
+        vis[u] = 1;
+        if(dep > tans){
+            tans = dep;
+            s = u;
+            cout << s << " " << tans << endl;
+        }
+        for(auto v : G[u]){
+            if(vis[v]) continue;
+            dfs(v, dep+1);
+        }
+    }
+    int MaxDiameter(int n, vector<int>& u, vector<int>& v) {
         // write code here
-        
+        int ans = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if(i == j) continue;
+                G[u[j]].pb(v[j]);
+                G[v[j]].pb(u[j]);
+            }
+            
+            memset(vis, 0, sizeof vis);
+            tans = -1; dfs(1, 0);
+            
+            memset(vis, 0, sizeof vis);
+            tans = -1; dfs(s, 0);
+            
+            ans = max(ans, tans);
+        }
+        return ans;
     }
 };
 int main(){
-    int n, m; cin >> n >> m;
-    vector<int> l(n);
-    for(int i = 0; i < m; i++) cin >> l[i];
+    int n; cin >> n;
+    vector<int> l(n), r(n);
+    for(int i = 0; i < n; i++) cin >> l[i];
+    for(int i = 0; i < n; i++) cin >> r[i];
 
     Solution *s = new Solution();
-    cout << s->oddnumber(n, m, l) << endl;
+    s->MaxDiameter(n, l, r);
+    return 0;
 }
 /*
-3 2
-1 2 3
+4
+1 2 3 4
+2 3 1 3
 */
