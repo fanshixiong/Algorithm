786aae4cffb239c360c5e941f054dcfb4fb1e25a
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-23 23:13:54 +0800

    nc

--- 牛客/练习赛74/E.cpp
+++ 牛客/练习赛74/E.cpp
@@ -1,13 +1,77 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define pi pair<int, int>
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
+const ll mod = 1023694381;
+
+vector<pi> G[510];
+ll dist[510][510];
+int a[510];
+
+ll qpow(ll a, ll b){
+	ll ans = 1;
+	while(b){
+		if(b & 1) ans = (ans * a) % mod;
+		a = (a * a) % mod;
+		b >>= 1;
+	}
+	return ans;
+}
+
+int n, m, K;
 void solve(){
-    int n; cin >> n;
+	cin >> n >> m >> K;
+	for(int i = 1; i <= n; i++) cin >> a[i];
+
+	memset(dist, 0x3f, sizeof dist);
+	for(int i = 0; i < m; i++){
+		int u, v, w;
+		cin >> u >> v >> w;
+		dist[u][v] = dist[v][u] = w;
+	}
+
+	for(int i = 1; i <= n; i++) dist[i][i] = 0;
+
+	for(int k = 1; k <= n; k++){
+		for(int i = 1; i <= n; i++)
+			for(int j = 1; j <= n; j++)
+				if(dist[i][j] > dist[i][k] + dist[k][j]) 
+					dist[i][j] = dist[i][k] + dist[k][j];
+	}
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j<=n; j++){
+			cout << dist[i][j] << " ";
+		}
+		cout <<endl;
+	}
+	ll ans = 0;
+	for(int i = 1; i <= n; i++){
+
+		if(a[i] == 1) {
+			ans = (ans + 1) % mod;
+			continue;
+		}
+		ll p = 0, cnt = 0, sums = 0;
+		for(int u = 1; u <= n; u++){
+			for(int v = 1; v <= n; v++){
+				if(v == u) continue;
+				sums++;
+				if(dist[u][v] == dist[u][i] + dist[i][v]) cnt++;
+			}
+		}
+
+		cout << sums << " " <<cnt <<endl;
+		p =  (sums-cnt) * qpow(sums, mod-2) % mod;
+
+		ans = (ans + 1ll + mod - qpow(p, K)) % mod;
+		cout << p << " " << ans << endl;
+	}
+	cout << ans << endl;
 }
 int main(){
-    IOS; int t; cin >> t;
+    IOS; int t = 1;
     while(t--){
         solve();
     }
--- 牛客/练习赛74/F.cpp
+++ 牛客/练习赛74/F.cpp
@@ -1,15 +1,56 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstring>
 using namespace std;
-#define ll long long
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 2e5 + 10;
-void solve(){
-    int n; cin >> n;
-}
-int main(){
-    IOS; int t; cin >> t;
-    while(t--){
-        solve();
+
+typedef long long LL;
+
+const int N = 505, P = 1023694381;
+
+int n, m, K, a[N], ans;
+
+LL d[N][N];
+
+int inline power(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) res = (LL)res * a % P;
+        a = (LL)a * a % P;
+        b >>= 1;
     }
-    return 0;
+    return res;
+}
+
+
+int main() {
+    memset(d, 0x3f, sizeof d);
+    scanf("%d%d%d", &n, &m, &K);
+    for (int i = 1; i <= n; i++) scanf("%d", a + i), d[i][i] = 0;
+    for (int i = 1; i <= m; i++) {
+        int u, v, w; scanf("%d%d%d", &u, &v, &w);
+        d[u][v] = d[v][u] = min(d[u][v], (LL)w);
+    }
+    for (int k = 1; k <= n; k++)
+        for (int i = 1; i <= n; i++)
+            for (int j = 1; j <= n; j++)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+    for (int i = 1; i <= n; i++) {
+        if (a[i] == 1) {
+            (ans += 1) %= P;
+            continue;
+        }
+        int s = 0, cur = 0;
+        for (int u = 1; u <= n; u++) {
+            for (int v = 1; v <= n; v++) {
+                if (u == v) continue;
+                s++;
+                if (d[u][v] == d[u][i] + d[i][v]) cur++;
+            }
+        }
+        cout << s <<" " << cur << endl;
+        int p = (LL)(s - cur) * power(s, P - 2) % P;
+        ans = (ans + 1ll + P - power(p, K)) % P;
+        cout << p << " " << ans<< endl;
+    }
+    printf("%d\n", ans);
 }
