81174d6eb3454256141b68259e7204d57729f0af
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-24 21:40:26 +0800

    nc

--- 牛客/练习赛74/F.cpp
+++ 牛客/练习赛74/F.cpp
@@ -1,56 +1,48 @@
-#include <iostream>
-#include <cstdio>
-#include <cstring>
+#include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
+const int maxn = 1e7 + 10;
+const ll mod = 1023694381;
 
-typedef long long LL;
+int n, m, L, R;
+ll fact[maxn], infact[maxn];
+ll qpow(ll a, ll b){
+	ll ans = 1;
+	while(b){
+		if(b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
 
-const int N = 505, P = 1023694381;
-
-int n, m, K, a[N], ans;
-
-LL d[N][N];
-
-int inline power(int a, int b) {
-    int res = 1;
-    while (b) {
-        if (b & 1) res = (LL)res * a % P;
-        a = (LL)a * a % P;
-        b >>= 1;
-    }
-    return res;
+	return ans;
 }
 
+ll C(ll n, ll m){
+	if(m == 0 || n == m) return 1ll;
+	return fact[n] * infact[m]  % mod * infact[n-m] % mod;
+}
+void solve(){
+    cin >> n >> m;
+    R = max(n, m);
+    L = min(n, m);
 
-int main() {
-    memset(d, 0x3f, sizeof d);
-    scanf("%d%d%d", &n, &m, &K);
-    for (int i = 1; i <= n; i++) scanf("%d", a + i), d[i][i] = 0;
-    for (int i = 1; i <= m; i++) {
-        int u, v, w; scanf("%d%d%d", &u, &v, &w);
-        d[u][v] = d[v][u] = min(d[u][v], (LL)w);
+    fact[1] = 1ll;
+    for(int i = 2; i <= R; i++) fact[i] = fact[i-1] * 1ll * i % mod;
+	
+	infact[R] = qpow(fact[R], mod - 2);
+    for (int i = R - 1; i; i--) infact[i] = infact[i + 1] * 1ll * (i + 1) % mod;
+
+    ll sums = qpow(m, n);
+	for(int i = 0; i < L; i++){
+		sums = (sums + mod - C(1ll*(n-1), 1ll*i) * C(1ll*m, 1ll*(i+1)) % mod * fact[i+1] % mod) % mod;
+	}
+	cout << sums << endl;
+}
+int main(){
+    IOS; int t = 1;
+    while(t--){
+        solve();
     }
-    for (int k = 1; k <= n; k++)
-        for (int i = 1; i <= n; i++)
-            for (int j = 1; j <= n; j++)
-                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
-    for (int i = 1; i <= n; i++) {
-        if (a[i] == 1) {
-            (ans += 1) %= P;
-            continue;
-        }
-        int s = 0, cur = 0;
-        for (int u = 1; u <= n; u++) {
-            for (int v = 1; v <= n; v++) {
-                if (u == v) continue;
-                s++;
-                if (d[u][v] == d[u][i] + d[i][v]) cur++;
-            }
-        }
-        cout << s <<" " << cur << endl;
-        int p = (LL)(s - cur) * power(s, P - 2) % P;
-        ans = (ans + 1ll + P - power(p, K)) % P;
-        cout << p << " " << ans<< endl;
-    }
-    printf("%d\n", ans);
+    return 0;
 }
