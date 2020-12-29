e881690a502cf1d62df406f221cdeeb6c75a0072
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2020-12-29 22:26:13 +0800

    nc

--- codeforces/#Round688/B.cpp
+++ codeforces/#Round688/B.cpp
@@ -3,24 +3,25 @@ using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
-int c[maxn];
+ll a[maxn], c[maxn];
+int n;
 void solve(){
-    int n, k;
-    cin >> n >> k;
-    for (int i = 1; i <= n; i++) cin >> c[i];
+    cin >> n;
+    for (int i = 1; i <= n; i++) cin >> a[i];
 
-    int res = 0x3f3f3f3f;
-    for (int maxp = 1; maxp <= 100; maxp++){
-        int ans = 0, i = 1;
-        while(i <= n && c[i] == maxp) i++;
-        while(i <= n){
-            ans++;
-            i += k;
-            while(i <= n && c[i] == maxp) i++;
-        }
-        res = min(res, ans);
+    ll ans = 1e18;
+    
+    ll sums = 0;
+    for(int i = 2; i <= n; i++)  c[i] = abs(a[i]-a[i-1]), sums += c[i];
+
+    for(int i = 2; i < n; i++){
+        ans = min(ans, sums - c[i] - c[i + 1] + abs(a[i+1] - a[i-1]));
+        // cout << c[i] << " " << sums - c[i] - c[i + 1] + abs(a[i+1] - a[i-1]) << endl;
     }
-    cout << res << endl;
+    ans = min(ans, sums - c[2]);
+    ans = min(ans, sums - c[n]);
+
+    cout << ans << endl;
 }  
 int main(){
     IOS; int t; cin >> t;
--- codeforces/#Round688/C.cpp
+++ codeforces/#Round688/C.cpp
@@ -2,30 +2,34 @@
 using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 2e5 + 10;
+const int maxn = 2e3 + 10;
+const int INF = 0x3f3f3f3f;
 
-// 从后往前找
+string a[maxn];
+
 void solve(){
-    int n, p, k;
-    cin >> n >> p >> k;
-    string a; cin >> a;
-
-    ll x, y;
-    cin >> x >> y;
-
-    ll ans = 1e18;
-    for(int i = n - k + 1; i <= n; i++){
-        ll res = 0;
-        for (int r = i; r >= 1; r-=k){
-            if(a[r-1] == '0') res += x;
-            ll t = r - p;
-            if(t < 0) break;
-            ll p = t * y + res;
-            ans = min(ans, p);
+    int ans[10] = {0};
+    int maxr[10] = {0}, minr[10];
+    int maxc[10] = {0}, minc[10];
+    for(int i = 0; i < 10; ++i) minr[i] = minc[i] = INF;
+    int n; cin >> n;
+    for(int i = 0; i < n; ++i) {
+        cin >> a[i];
+        for(int j = 0; j < n; ++j) {
+            int x = a[i][j] - '0';
+            maxr[x] = max(maxr[x], i), minr[x] = min(minr[x], i);
+            maxc[x] = max(maxc[x], j), minc[x] = min(minc[x], j);
         }
     }
-    
-    cout << ans << endl;
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            int x = a[i][j] - '0';
+            ans[x] = max(ans[x], max(i-minr[x], maxr[x]-i) * max(n-j-1, j));
+            ans[x] = max(ans[x], max(j-minc[x], maxc[x]-j) * max(n-i-1, i));
+        }
+    }
+    for(int i = 0; i < 10; ++i) cout << ans[i] << " ";
+    cout << endl;
 }
 int main(){
     IOS; int t; cin >> t;
