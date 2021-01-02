962696a01021d4d8c1b4d56fbe162f4ece0196df
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-01-02 22:50:27 +0800

    nc 75

--- 1.cpp
+++ 1.cpp
@@ -1,9 +1,32 @@
-#include<bits/stdc++.h>
+#include<stdio.h>
+#define mod 1000000007
 
-using namespace std;
+long long fastPower(long long n, long long p)
+{
+    long long ans = 1;
+    while (p!=0)
+    {
+        if (p & 1 == 1)ans = (ans * n) % mod;
+        n = (n * n) % mod;
+        p >>= 1;
+    }
+    return ans%mod;
+}
 
-
-int main(){
-	cout << 1 << endl;
-	return 0;
+int main()
+{
+    int a,b,m,n;
+    scanf("%d %d %d %d",&a,&b,&m,&n);
+    long long n1=1;
+    long long n2=1;
+    long long fn = 0;
+    if(n==1||n==2)fn=1;
+    for(int i=2;i<n;i++)
+    {
+        fn = ((a*n2)+(b*n1))%(mod-1);
+        n1 = n2;
+        n2 = fn;
+    }
+    printf("%lld\n", fn);
+    printf("%lld",fastPower(m,fn));
 }
--- 牛客/练习赛75/A.cpp
+++ 牛客/练习赛75/A.cpp
@@ -1,65 +1,32 @@
+//https://oi-wiki.org/math/quick-pow/
+
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
 const int maxn = 2e5 + 10;
-ll a[maxn];
+const ll mod = 1e9+7;
+ll a, b, m, n;
+ll qpow(ll a, ll b){
+	ll ans = 1;
+	while(b){
+		if(b & 1) ans = ans * a % mod;
+		a = a * a % mod;
+		b >>= 1;
+	}
+	return ans % mod;
+}
 void solve(){
-    int n; cin >> n;
-    for(int i = 0; i < n; i++) cin >> a[i];
-    bool flag = true;
-	ll t = a[1] - a[0];
-	for (int i = 0; i < n - 1; ++i){
-    	if (a[i + 1] - a[i] != t){
-        	flag = false;
-        	break;
-    	}
-	}
-	if(flag){
-		cout << "YES" << endl;
-		return;
-	}
-	
-	flag = true;
-	if(a[1] % a[0] != 0) flag = false;
-	t = a[1] / a[0];
-	for (int i = 0; i < n - 1; ++i){
-		if(a[i] * t != a[i+1]){
-        	flag = false;
-        	break;
-    	}
-	}
-	if(flag){
-		cout << "YES" << endl;
-		return;
-	}
-
-	flag = true;
-	if(a[0] % a[1] != 0) flag = false;
-	t = a[0] / a[1];
-	for (int i = 0; i < n - 1; ++i){
-		if(a[i] != a[i+1] * t){
-        	flag = false;
-        	break;
-    	}
-	}
-	if(flag){
-		cout << "YES" << endl;
-		return;
-	}
-	
-	flag = true;
-	t = a[1] % a[0];
-	for (int i = 0; i < n - 1; ++i){
-		if(a[i+1] < a[i] || a[i+1] % a[i] != t){
-        	flag = false;
-        	break;
-    	}
-	}
-	if(flag) cout << "YES" << endl; 
-	else cout << "NO" << endl;
-
-	// cout << 3 % 7 << " " << 7 % 4 << endl; 
+    cin >> a >> b >> m >> n;
+    ll f1 = 1, f2 = 1, fn = 1;
+    for(int i = 3; i <= n; i++){
+    	fn = (a * f1 + b *  f2)  % (mod - 1); // 为什么是mod-1？ 费马小定理
+    	f2 = f1;
+    	f1 = fn;
+    	// cout << f1 << " " << f2 << endl;
+    }
+    cout << fn << endl;
+    cout << qpow(m, fn) << endl;
 }
 int main(){
     IOS; int t = 1;
