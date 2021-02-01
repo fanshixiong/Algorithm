3027780b39d612237d8d326fe4aafe429e2d5298
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-02-01 22:54:58 +0800

    nc

--- 牛客/寒假训练营/第一场/B.cpp
+++ 牛客/寒假训练营/第一场/B.cpp
@@ -5,6 +5,24 @@ using namespace std;
 const int maxn = 2e5 + 10;
 const ll mod = 1e9 + 7;
 void solve(){
+	ll k; cin >> k;
+	if(k == 0){
+		cout << "(" << endl;
+		return;
+	}
+	ll a = 1e9, b = 1e9, p = 1e9;
+	for(ll i = 1; i * i <= k; i++){
+		ll x = i, y = k / i, z = k - (i * (k / i));
+		// cout <<  x << " " << y << " " << z << endl;
+		if(x + y + z < a + b + p){
+			a = x, b = y, p = z;
+		}
+	}
+	string ans = "(";
+	for(int i = 0; i < p; i++) ans += ")";
+	for(int i = 0; i < a - 1; i++) ans += "(";
+	for(int i = 0; i < b; i++) ans += ")";
+	cout << ans << endl;
 }
 int main(){
     IOS; int t = 1;
--- 牛客/寒假训练营/第一场/F.cpp
+++ 牛客/寒假训练营/第一场/F.cpp
@@ -5,6 +5,16 @@ using namespace std;
 const int maxn = 2e5 + 10;
 const ll mod = 1e9 + 7;
 void solve(){
+	int n;
+	cin >> n;
+	char nn[110], nm[110];
+	for(int i = 0; i < n; i++) cin >> nn[i];
+	for(int i = 0; i < n; i++) cin >> nm[i];
+
+	int cnt = 0;
+	for(int i = 0; i < n; i++) if(nn[i] == nm[i]) cnt++;
+
+	cout << cnt + n << " " << 0 << endl;
 }
 int main(){
     IOS; int t = 1;
