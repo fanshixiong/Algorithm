8b3475f371cd0723f3a95214486ca15ac66471f8
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-03-03 20:03:22 +0800

    nc

--- 牛客/寒假训练营/第四场/A.cpp
+++ 牛客/寒假训练营/第四场/A.cpp
@@ -5,6 +5,22 @@ using namespace std;
 const int maxn = 2e5 + 10;
 const ll mod = 1e9 + 7;
 void solve(){
+	int n, m;
+	cin >> n >> m;
+	int x; 
+	string s;
+	vector<int> cnt(m + 1, 0), sum(m + 1, 0);
+	vector<bool> ans(m + 1, false);
+	for(int i = 0; i < n; i++){
+		cin >> x >> s;
+		if(s[0] == 'A') cnt[x]++;
+		sum[x]++;
+		if(cnt[x] * 2 < sum[x]) ans[x] = true;
+	}
+	bool flg = false;
+	for(int i = 1; i <= m; i++) if(!ans[i]) cout << i << " ", flg = true;
+	if(!flg) cout << -1;
+	cout << endl;
 }
 int main(){
     IOS; int t = 1;
