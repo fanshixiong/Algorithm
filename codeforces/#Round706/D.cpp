9d417c9605e6acca464cdcbff660b5477e8ded46
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-03-12 20:57:36 +0800

    阿里巴巴笔试

--- codeforces/#Round706/D.cpp
+++ codeforces/#Round706/D.cpp
@@ -1,15 +1,68 @@
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 3e5 + 10;
-int a[maxn], b[maxn];
-void solve(){
+#define pi pair<int, int>
+const int maxn = 1010;
+vector<int> G[maxn];
+int vis[maxn];
+int dist[maxn][maxn];
+int graph[maxn][maxn];
+/*
+int main() {
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int a, b; cin >> a >> b;
+        G[a].push_back(b);
+    }
+    memset(dist, 0x3f, sizeof dist);
+
+    int q; cin >> q;
+    queue<pi> qu;
+    while(q--){
+        int a, b; cin >> a >> b;
+        memset(vis, 0, sizeof vis);
+        qu.push({a, 0}); vis[a] = 1;
+        while(!qu.empty()){
+            auto x = qu.front(); qu.pop();
+            int u = x.first, d = x.second;
+            if(u == b){
+                cout << d << endl;
+                break;
+            }
+            //cout << u << " ss " << endl;
+            for(auto v : G[u]){
+                if(vis[v]) continue;
+                qu.push({v, d+1});
+                vis[v] = 1;
+            }
+        }
+        cout << -1 << endl;
+    }
+    return 0;
 }
-int main(){
-    IOS; int t = 1;
-    while(t--){
-        solve();
+*/
+int main() {
+    int n, m; cin >> n >> m;
+    memset(dist, 0x3f, sizeof dist);
+    for(int i = 0; i < m; i++){
+        int a, b; cin >> a >> b;
+        dist[a][b] = 1;
+    }
+
+    for(int k = 1; k <= n; k++){
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                dist[i][j] = min(dist[i][k] + dist[k][j], dist[i][j]);
+            }
+        }
+    }
+
+    int q; cin >> q;
+    while(q--){
+        int a, b; cin >> a >> b;
+        
+        cout << (dist[a][b] == 0x3f3f3f3f ? -1 : dist[a][b]) << endl;
     }
     return 0;
 }
--- codeforces/#Round706/E.cpp
+++ codeforces/#Round706/E.cpp
@@ -1,13 +1,29 @@
+// 本题为考试单行多行输入输出规范示例，无需提交，不计分。
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define IOS ios_base::sync_with_stdio(0); cin.tie(0);cout.tie(0);
-const int maxn = 3e5 + 10;
-int a[maxn], b[maxn];
+#define pi pair<int, int>
+const int maxn = 1e5 + 10;
+int xi[maxn], yi[maxn];
+map<pi, int> p;
 void solve(){
+    int n, m; cin >> n >> m;
+    for(int i = 1; i <= m; i++){
+        cin >> xi[i] >> yi[i];
+        p[{xi[i], yi[i]}]++;
+    }
+    int ans = 0;
+    for(int i = 1; i <= m; i++){
+        if(p[{xi[i], yi[i]}] && p[{yi[i], xi[i]}]) {
+            ans += 2;
+            p[{xi[i], yi[i]}]--;
+        }
+        else ans += 1;
+    }
+    cout << ans << endl;
 }
-int main(){
-    IOS; int t = 1;
+int main() {
+    int t; cin >> t;
     while(t--){
         solve();
     }
