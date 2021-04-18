7dd777751e97441c1b97409f42a383a2b8c0196f
fanshixiong <32351816+fanshixiong@users.noreply.github.com>
2021-04-18 21:45:04 +0800

    笔试

--- 笔试/腾讯音乐笔试/1.cpp
+++ 笔试/腾讯音乐笔试/1.cpp
@@ -1,20 +1,31 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int maxn = 1e4 + 10;
-int a[maxn];
+int a[maxn], b[maxn];
 void solve(){
-	int n; cin >> n;
-	for(int i = 0; i < n; i++) cin >> a[i];
-	unordered_map<int, int> p;
-	for(int i = 0; i < n; i++) p[a[i]]++;
+	int n, m; cin >> n >> m;
+	for(int i = 1; i <= n; i++) cin >> a[i];		
 
-	int ans = 0x3f3f3f3f;
-	for(int i = 0; i < n; i++) if(p[a[i]] == 1) ans = min(ans, a[i]);\
+	for(int i = 1; i <= m; i++) cin >> b[i];
 
-	if(ans == 0x3f3f3f3f) cout << -1 << endl;
-	else cout << ans << endl;
+	int sums = 0;
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++) sums = sums ^ (a[i] ^ b[j]);
+	}
+
+	int sum = 0;
+	for(int i = 1; i <= m; i++) sum ^= b[i];
+
+	cout << sums << " " << sum << endl;
 }
+/*
+5
+4 4
+1 2 3 4
+3 4 5 6
+*/
 int main(){
+	cout << (12 ^ 4) << endl;
 	int t; cin >> t;
 	while(t--){
 		solve();
